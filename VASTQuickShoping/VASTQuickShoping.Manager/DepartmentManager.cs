7d4ea44fc5a0b7b9360a3fd37388260b68f61667
jhonydaniel <jhonydanielsalazar53@gmail.com>
2020-10-24 20:41:11 -0500

    Tabla department
    realizado

---
+++ VASTQuickShoping/VASTQuickShoping.Manager/DepartmentManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VASTQuickShoping.Manager.Contracts;
+using VASTQuickShoping.Models;
+using VASTQuickShoping.Models.Domain;
+
+namespace VASTQuickShoping.Manager
+{
+    public class DepartmentManager : IDepartmentManager
+    {
+      
+
+        public Department Get(int id)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                return context.Departments.Where(k => k.DepartmentID == id).SingleOrDefault();//busca un registrp y devuelve un objeto caundo no va coincidir ningun id estamos haciendo una busqueda por idpara modifi
+
+            }
+        }
+
+        public IEnumerable<Department> GetAll(bool status)
+        {
+            var context = new ApplicationDbContext();
+            var lista = context.Departments.ToList();
+            return lista;
+        }
+
+        public int Insert(Department obj)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                context.Entry(obj).State = EntityState.Added;
+                return context.SaveChanges();
+            }
+        }
+
+        public int Update(Department obj)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                context.Entry(obj).State = EntityState.Modified;
+                return context.SaveChanges();
+            }
+        }
+
+        public int Delete(int id)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                Department obj = context.Departments.Find(id);
+                context.Entry(obj).State = EntityState.Deleted;
+                return context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ VASTQuickShoping/VASTQuickShoping.UI/Areas/Admin/Controllers/DepartmentController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VASTQuickShoping.Manager;
+using VASTQuickShoping.Models.Domain;
+using VASTQuickShoping.UI.Common;
+
+namespace VASTQuickShoping.UI.Areas.Admin.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: Admin/Department
+        public ActionResult Index()
+        {
+            var data = new DepartmentManager().GetAll(true);
+            return View(data);
+        }
+
+
+        [HttpGet]
+        public ActionResult Insertar()
+        {
+            ViewBag.op = CRUD.Insertar.ToString();
+            return View("Formulario4", new Department());//un objeto categorua para que el objeto no quede nulo
+        }
+        [HttpPost]
+        public ActionResult Insertar(Department obj)
+        {
+            if (ModelState.IsValid)
+            {
+                int rpta = new DepartmentManager().Insert(obj);//despues de ingreesar el dato nos dirija al index
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Formulario4", obj);//que lo muestre los datos que el usuario mismo a ingresado
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult Modificar(int id)
+        {
+            ViewBag.op = CRUD.Modificar.ToString();
+            return View("Formulario4", new DepartmentManager().Get(id));//un objeto categorua para que el objeto no quede nulo
+        }
+        [HttpPost]
+        public ActionResult Modificar(Department obj)
+        {
+            if (ModelState.IsValid)
+            {
+                int rpta = new DepartmentManager().Update(obj);//despues de ingreesar el dato nos dirija al index
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Formulario4", obj);//que lo muestre los datos que el usuario mismo a ingresado
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Eliminar(int id)
+        {
+            //un objeto categorua para que el objeto no quede nulo
+            _ = new DepartmentManager().Delete(id);
+            return RedirectToAction("index");
+
+        }
+
+
+    }
+
+        }
